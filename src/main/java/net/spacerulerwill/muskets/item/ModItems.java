d35e9ccda2c9a427ea6ea2ec053ffdd28d0dffab
William Redding <williamdredding@gmail.com>
2023-12-17 13:31:57 -0800

    Texture changes

--- src/main/java/net/spacerulerwill/muskets/item/ModItems.java
+++ src/main/java/net/spacerulerwill/muskets/item/ModItems.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.BowItem;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
