09bd77efb49affc3afecd98d1818dc78e76f88dc
William Redding <williamdredding@gmail.com>
2023-12-17 10:24:46 -0800

    Musket item and texture

--- src/main/java/net/spacerulerwill/muskets/Muskets.java
+++ src/main/java/net/spacerulerwill/muskets/Muskets.java
@@ -2,6 +2,7 @@ package net.spacerulerwill.muskets;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.spacerulerwill.muskets.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class Muskets implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/spacerulerwill/muskets/item/ModItems.java
@@ -0,0 +1,31 @@
+package net.spacerulerwill.muskets.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.spacerulerwill.muskets.Muskets;
+
+public class ModItems {
+
+    public static final Item MUSKET = registerItem("musket", new Item(new FabricItemSettings()));
+
+    private static void addItemToWeaponsCombatGroup(FabricItemGroupEntries entries) {
+        entries.add(MUSKET);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(Muskets.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Muskets.LOGGER.info("Registering mod items for " + Muskets.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(ModItems::addItemToWeaponsCombatGroup);
+    }
+
+}
