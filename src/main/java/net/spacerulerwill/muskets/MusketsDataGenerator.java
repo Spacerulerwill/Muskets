7e751574be9e79ac3d653e88486822197bf4d2f3
William Redding <williamdredding@gmail.com>
2023-12-18 16:45:01 -0800

    Advancement

--- src/main/java/net/spacerulerwill/muskets/MusketsDataGenerator.java
+++ src/main/java/net/spacerulerwill/muskets/MusketsDataGenerator.java
@@ -2,8 +2,14 @@ package net.spacerulerwill.muskets;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
+import net.minecraft.advancement.AdvancementEntry;
+
+import java.util.function.Consumer;
 
 public class MusketsDataGenerator implements DataGeneratorEntrypoint {
+
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
